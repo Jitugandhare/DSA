298d6d3ab4c87b638da9acbc9efe13682b0f5efb
Jitu gandhare <jitugandhare@gmail.com>
2025-02-06 11:16:48 +0530

    woodcutter problem

---
+++ DSA/woodCutter.js
@@ -0,0 +1,28 @@
+function maxHeight(trees, M) {
+    let l = 0;
+    let r = Math.max(...trees);
+    let result = 0;
+
+    function getWood(h) {
+        return trees.reduce((total, tree) => total + Math.max(0, tree - h), 0);
+    }
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2);
+        let wood = getWood(mid);
+
+        if (wood >= M) {
+            result = mid;
+            l = mid + 1; // Try to find a higher height
+        } else {
+            r = mid - 1; // We need to cut lower
+        }
+    }
+
+    return result;
+}
+
+let trees = [20, 15, 10, 17];
+let M = 7;
+console.log(maxHeight(trees, M));
+
