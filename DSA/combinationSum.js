6104f5249a834ef21e7ae4c87581adaa87b77301
Jitu gandhare <jitugandhare@gmail.com>
2025-05-09 00:27:57 +0530

    user Schema is ready

---
+++ DSA/combinationSum.js
@@ -0,0 +1,24 @@
+var combinationSum = function(candidates, target) {
+    let result = [];
+
+    const backtrack = (remain, combo, start) => {
+        if (remain === 0) {
+            result.push([...combo]);
+            return;
+        }
+        if (remain < 0) return;
+
+        for (let i = start; i < candidates.length; i++) {
+            combo.push(candidates[i]);
+            backtrack(remain - candidates[i], combo, i); 
+            combo.pop(); 
+        }
+    };
+
+    backtrack(target, [], 0);
+    return result;
+};
+
+let candidates = [2, 3, 6, 7];
+let target = 7;
+console.log(combinationSum(candidates, target));
